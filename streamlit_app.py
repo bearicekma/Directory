351f29fa697f8e3a845d2ec02579807ade071e0e
bearicekma <149989096+bearicekma@users.noreply.github.com>
2023-11-11 22:40:07 +0900

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,8 @@
 # モジュールの読み込み
 import pandas as pd
 import streamlit as st
-import pygwalker as pyg
 
 st.title('Streamlitへようこそ')
 
 # 初期設定
 st.set_page_config(layout="wide")
-
-# CSVファイル取得（サイドパネル）
-df = None
-with st.sidebar:
-    input = st.file_uploader("Choose a CSV file")
-    if input is not None:
-        df = pd.read_csv(input)
-
-# Graphic Walker 操作（メインパネル）
-if df is not None:
-    output = pyg.walk(df, env='Streamlit')
-    st.write(output)
