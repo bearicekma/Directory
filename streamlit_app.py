1413a6f1f351bd018a900333dc5e96374f477935
bearicekma <149989096+bearicekma@users.noreply.github.com>
2023-11-11 21:21:32 +0900

    Colaboratory を使用して作成しました

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "mount_file_id": "1xMphHk5BfeKCMg3gUsEGjVpKO4DZmxsr",
-      "authorship_tag": "ABX9TyOJM7DlNx7/dIPZ+ABTJCh6",
+      "authorship_tag": "ABX9TyMdirlN0OptQaDE2k5KkdfJ",
       "include_colab_link": true
     },
     "kernelspec": {
@@ -35,38 +35,28 @@
         "import streamlit as st\n",
         "import pygwalker as pyg\n",
         "\n",
-        "# 初期設定\n",
-        "st.set_page_config(layout=\"wide\")\n",
+        "st.title('Streamlitへようこそ')\n",
         "\n",
-        "# CSVファイル取得（サイドパネル）\n",
-        "df = None\n",
-        "with st.sidebar:\n",
-        "    input = st.file_uploader(\"Choose a CSV file\")\n",
-        "    if input is not None:\n",
-        "        df = pd.read_csv(input)\n",
+        "# # 初期設定\n",
+        "# st.set_page_config(layout=\"wide\")\n",
         "\n",
-        "# Graphic Walker 操作（メインパネル）\n",
-        "if df is not None:\n",
-        "    output = pyg.walk(df, env='Streamlit')\n",
-        "    st.write(output)"
+        "# # CSVファイル取得（サイドパネル）\n",
+        "# df = None\n",
+        "# with st.sidebar:\n",
+        "#     input = st.file_uploader(\"Choose a CSV file\")\n",
+        "#     if input is not None:\n",
+        "#         df = pd.read_csv(input)\n",
+        "\n",
+        "# # Graphic Walker 操作（メインパネル）\n",
+        "# if df is not None:\n",
+        "#     output = pyg.walk(df, env='Streamlit')\n",
+        "#     st.write(output)"
       ],
       "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "uolL8zF4bIW-",
-        "outputId": "40155a07-5615-4d46-977f-8fff0439c6ef"
+        "id": "uolL8zF4bIW-"
       },
       "execution_count": null,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "Writing streamlit_app.py\n"
-          ]
-        }
-      ]
+      "outputs": []
     }
   ]
 }
